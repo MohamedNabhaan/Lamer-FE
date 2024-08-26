31db82b16e114d5b410360b97582881f0f9c48f4
mali0034 <nabhaanali8000@gmail.com>
2024-08-26 21:20:44 +0530

    Added Navbar and page sections

---
+++ src/index.js
@@ -0,0 +1,44 @@
+export const NAV_ITEMS = [
+    {
+        label : "Home",
+        href : ""
+
+        },
+    {
+        label : "Services",
+        href : ""
+        },
+    {
+        label : "Projects",
+        href : ""
+        },
+    {
+        label : "About Us",
+        href : "",
+        children : [
+            {
+                label : "About Us",
+                href : ""
+                },
+            {
+                label : "Our Team",
+                href : ""
+                }
+        ]
+        },
+    {
+        label : "Career",
+        href : ""
+     },
+    {
+        label : "Contact Us",
+        href : ""
+        },
+]
+
+// const NAV_ITEMS = ['Home','Services','Projects','About Us','Career','Contact Us']
+
+// const SUB_NAV_ITEMS = [
+//     ABOUT_US = ["About Us","Our Team"],
+
+// ]
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import Navbar  from "../../components/Navbar"
+
+export default function Home (){
+    return (
+        <Box>
+        <Navbar/>
+
+        <h1>LAMER group : Land and Marine Environmental Resource Group</h1>
+        </Box>
+    );
+}
