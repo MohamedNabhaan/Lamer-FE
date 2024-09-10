120fb36fc779662e6eb95f1ee912e1d16fb3ba95
mali0034 <nabhaanali8000@gmail.com>
2024-09-10 12:16:43 +0500

    cleaned up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home  from "./pages/Home/Home"
-import AboutUs from './pages/AboutUs/AboutUs'
-import Navbar  from "./components/Navbar"
-import RootPage from './Root'
-import Services from './pages/Services/Services'
-import Clients from './pages/Clients/Clients'
-import Team from './pages/Team/Team'
-import Careers from './pages/Careers/Careers'
-import Projects from './pages/Projects/Projects'
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import RootPage from "./Root";
+import Services from "./pages/Services/Services";
+import Clients from "./pages/Clients/Clients";
+import Team from "./pages/Team/Team";
+import Careers from "./pages/Careers/Careers";
+import Projects from "./pages/Projects/Projects";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<RootPage/>,
+    path: "/",
+    element: <RootPage />,
     children: [
-      { path:'', element: <Home/>},
-      { path:'Services', element: <Services/>},
-      { path: 'Clients', element: <Clients/>},
-      { path: 'Projects', element: <Projects/>},
-      { path: 'AboutUs', element: <AboutUs/>},
-      { path: 'OurTeam', element: <Team/>},
-      { path: 'Careers', element: <Careers/>},
-    ]
-  }
-])
+      { path: "", element: <Home /> },
+      { path: "Services", element: <Services /> },
+      { path: "Clients", element: <Clients /> },
+      { path: "Projects", element: <Projects /> },
+      { path: "AboutUs", element: <AboutUs /> },
+      { path: "OurTeam", element: <Team /> },
+      { path: "Careers", element: <Careers /> },
+    ],
+  },
+]);
 
 function App() {
-  
-
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
