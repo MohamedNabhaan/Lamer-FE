9da999e79a24352d4a03c7bfb2ade1238895c78e
mali0034 <nabhaanali8000@gmail.com>
2024-09-12 16:23:02 +0500

    added initial design for about-us page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,11 +1,32 @@
-import { Box,Heading } from "@chakra-ui/react";
+import { Box, Heading, Container, Stack, Text } from "@chakra-ui/react";
 
-
-
-export default function AboutUs (){
-    return (
-        <Box>
-            <Heading> About US</Heading>
-        </Box>
-    );
+export default function AboutUs() {
+  return (
+    <Box>
+      <Container
+        maxW={"container.xl"}
+        paddingTop={12}
+        paddingLeft={20}
+        paddingBottom={12}
+        borderBottom={"solid"}
+        borderColor={"design.200"}
+        justifyContent={"center"}
+      >
+        <Heading
+          as="h1"
+          size={"3xl"}
+          fontWeight={500}
+          color={"brand.400"}
+          display={"block"}
+        >
+          About Us
+        </Heading>
+      </Container>
+      <Box paddingInline={16} paddingTop={8} height={"60vh"}>
+        <Heading as="h2" size={"lg"}>
+          Company History
+        </Heading>
+      </Box>
+    </Box>
+  );
 }
