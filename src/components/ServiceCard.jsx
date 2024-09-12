2e8cce98725248b5f5ab2a8d4dfc5a42072fcd4d
mali0034 <nabhaanali8000@gmail.com>
2024-09-12 16:23:24 +0500

    created design for service page

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,56 @@
+import { Box, Text, Image, Collapse, useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+
+export default function ServiceCard(props) {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Box>
+      <Box
+        height={"32vh"}
+        position={"relative"}
+        bgColor={"black"}
+        overflow={"hidden"}
+        onClick={onToggle}
+      >
+        <Text
+          position={"absolute"}
+          color={"white"}
+          w={"100%"}
+          zIndex={1}
+          bottom={2}
+          left={4}
+          fontSize={"xl"}
+        >
+          {props.service.label}
+        </Text>
+
+        <Image
+          transform={"scale(1.10)"}
+          maxH={"100%"}
+          h={"100%"}
+          opacity={0.4}
+          w={"100%"}
+          src={props.service.img}
+          transition={"ease-in"}
+          transitionDuration={"600ms"}
+          _hover={{
+            transform: "scale(1.03)",
+            opacity: 1,
+            cursor: "pointer",
+          }}
+        ></Image>
+      </Box>
+      <Collapse in={isOpen}>
+        <Text
+          bgColor={"brand.400"}
+          borderBottomRadius={16}
+          padding={4}
+          color={"white"}
+        >
+          {props.service.description}
+        </Text>
+      </Collapse>
+    </Box>
+  );
+}
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,11 +1,53 @@
-import { Box,Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Stack,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import ServiceCard from "../../components/ServiceCard";
+import { SERVICE_DETAILS } from "../../index.js";
+import { useEffect } from "react";
 
-
-
-export default function Services (){
-    return (
-        <Box>
-            <Heading>Services</Heading>
-        </Box>
-    );
+export default function Services() {
+  return (
+    <Box overflow={"hidden"}>
+      <Container
+        maxW={"container.xl"}
+        paddingTop={12}
+        paddingLeft={20}
+        paddingBottom={16}
+        borderBottom={"solid"}
+        borderColor={"design.200"}
+        justifyContent={"center"}
+      >
+        <Stack direction={"column"} spacing={10}>
+          <Heading
+            as="h1"
+            size={"3xl"}
+            fontWeight={500}
+            color={"brand.400"}
+            display={"block"}
+          >
+            Services
+          </Heading>
+          <Text fontSize={"2xl"}>
+            To ensure our clients recieve the best possible solutions, we offer
+            a variety of specific services.
+          </Text>
+        </Stack>
+      </Container>
+      <SimpleGrid
+        columns={{ base: 1, md: 2 }}
+        spacing={4}
+        paddingTop={8}
+        paddingInline={16}
+      >
+        {SERVICE_DETAILS.map((service) => (
+          <ServiceCard service={service} />
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
 }
