723079a07a26b14f6c2d6518ad83ace5fbf44396
mali0034 <nabhaanali8000@gmail.com>
2024-09-12 16:21:50 +0500

    added auto page top navigation from the navbar

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-export default function RootPage (){
-    return (
-        <>
-        <Navbar></Navbar>
-        <Outlet/>
-        <Footer></Footer>
-        </>
-    );
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+export default function RootPage() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return (
+    <>
+      <Navbar></Navbar>
+      <Outlet />
+      <Footer></Footer>
+    </>
+  );
 }
