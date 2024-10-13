a1296ea7c7b2e06a991901d9aab94986477a8349
mali0034 <nabhaanali8000@gmail.com>
2024-10-13 14:26:28 +0500

    Created teams page

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,6 +1,49 @@
-import { Box, Heading, Container, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Container,
+  Stack,
+  Text,
+  SimpleGrid,
+  Image,
+  Flex,
+  Center,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import pfp from "../../assets/pfp.png";
 
 export default function Team() {
+  let [employees, setEmployees] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    async function fetchEmployees() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/employee");
+
+      const resData = await response.json();
+
+      resData.map((data) => {
+        const vals = data.displayPic
+          .replace("[", "")
+          .replace("]", "")
+          .replace(/["]/g, "")
+          .split(",");
+        data.displayPic = vals;
+      });
+
+      setEmployees(resData);
+      setIsFetching(false);
+    }
+
+    fetchEmployees();
+  }, []);
+
+  function handleClick() {
+    setClicked(!clicked);
+  }
+
   return (
     <Box minH={"72vh"}>
       <Container
@@ -24,9 +67,95 @@ export default function Team() {
           >
             Our Team
           </Heading>
+          <Text>
+            We are a highly committed team of environmental engineers, analysts,
+            surveyors, environmental scientists as well as marine
+            ecologists/biologists dedicated in helping you make environmentally
+            sound decisions through systematic research and open communication
+            with stakeholders.
+          </Text>
+          <Text>
+            Our team members have extensive experience with significant projects
+            from public and private sector development. The key members of the
+            firm have been involved in national and international level coral
+            reef conservation and management initiatives and have expertise and
+            experience assessing and recommending management options in marine
+            and coastal environmental issues .
+          </Text>
         </Stack>
       </Container>
-      <Text>LOl</Text>
+      <Box>
+        {isFetching ? (
+          <Stack>
+            <Heading padding={12} textAlign={"center"} color={"brand.400"}>
+              Loading.....
+            </Heading>
+          </Stack>
+        ) : (
+          <Container maxW={"container.lg"} padding={0}>
+            <Flex
+              display={"flex"}
+              flexWrap={"wrap"}
+              justifyContent={"space-evenly"}
+            >
+              {employees.map((employee) => {
+                return (
+                  <Stack
+                    flexBasis={{ base: "100%", sm: "50%", md: "33.33" }}
+                    padding={8}
+                  >
+                    <Box
+                      width={{ base: "60vh", md: "20vh" }}
+                      height={{ base: "70vh", md: "30vh" }}
+                      justifyContent={{ base: "center", md: "flex-start" }}
+                      alignItems={{ base: "center", md: "flex-start" }}
+                    >
+                      <Image
+                        borderRadius={10}
+                        width={"100%"}
+                        height={"100%"}
+                        objectFit={"cover"}
+                        src={employee.displayPic}
+                        fallbackSrc={pfp}
+                        boxShadow={"lg"}
+                      ></Image>
+                    </Box>
+                    <Box paddingBlock={2}>
+                      <Text
+                        fontSize={"xl"}
+                        fontWeight={"800"}
+                        paddingTop={1}
+                        paddingLeft={2}
+                      >
+                        {employee.name}
+                      </Text>
+                      <Text
+                        fontSize={"lg"}
+                        fontWeight={"500"}
+                        paddingBottom={1}
+                        paddingLeft={2}
+                      >
+                        {employee.position}
+                      </Text>
+
+                      <Text
+                        fontSize={"md"}
+                        color={"gray.500"}
+                        fontWeight={"500"}
+                        paddingTop={2}
+                        paddingBottom={1}
+                        paddingLeft={2}
+                      >
+                        {employee.qualifications}
+                      </Text>
+                    </Box>
+                  </Stack>
+                );
+              })}
+            </Flex>
+          </Container>
+        )}
+      </Box>
     </Box>
   );
 }
