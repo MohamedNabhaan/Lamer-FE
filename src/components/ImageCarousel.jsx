f4b70947d68e42ee7d4ad016598b4b0e853dbe30
mali0034 <nabhaanali8000@gmail.com>
2024-10-16 11:57:45 +0500

    Made Introduction carousel movement vertical

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -10,10 +10,14 @@ const settings = {
   speed: 2000,
   slidesToShow: 1,
   slidesToScroll: 1,
-  infinite: true,
+
   fade: false,
+  vertical: true,
+  draggable: false,
   autoplay: true,
+  lazyLoading: true,
   autoplaySpeed: 4500,
+
   dotsClass: "vertical-dots",
 };
 
@@ -31,43 +35,42 @@ export default function ImageCarousel(props) {
   const fadeInAnimation = `${fadeIn} 5s`;
   return (
     <>
-      <Box>
-        <Slider {...settings}>
-          {props.images.map((image) => (
-            <Flex width={"100%"} maxH={"600px"} overflow={"hidden"}>
-              <Text
-                letterSpacing={0}
-                whiteSpace={"pre-line"}
-                flexWrap={"wrap"}
-                fontSize={{ base: "md", md: "4xl", lg: "5xl" }}
-                position={"absolute"}
-                color="white"
-                zIndex={999}
-                paddingTop={{ base: 16, md: 24, lg: 32 }}
-                paddingLeft={{ base: 10, md: 12, lg: 16 }}
-              >
-                {image.heading}
-              </Text>
-              <Text
-                fontSize={{ base: "md", md: "xl", lg: "2xl" }}
-                position={"absolute"}
-                color={"design.300"}
-                paddingTop={{ base: 20, md: 36, lg: 48 }}
-                paddingLeft={{ base: 10, md: 12, lg: 20 }}
-                transition={"fade"}
-                transitionDuration={"600ms"}
-              >
-                {image.subHeading}
-              </Text>
-              <Image
-                w={"100%"}
-                height={{ base: "300px", md: "600px" }}
-                src={image.image}
-              />
-            </Flex>
-          ))}
-        </Slider>
-      </Box>
+      <Slider {...settings}>
+        {props.images.map((image) => (
+          <Flex height={"80vh"} overflow={"hidden"}>
+            <Text
+              letterSpacing={0}
+              whiteSpace={"pre-line"}
+              flexWrap={"wrap"}
+              fontSize={{ base: "md", md: "4xl", lg: "5xl" }}
+              position={"absolute"}
+              color="white"
+              zIndex={999}
+              paddingTop={{ base: 16, md: 24, lg: 32 }}
+              paddingLeft={{ base: 10, md: 12, lg: 16 }}
+            >
+              {image.heading}
+            </Text>
+            <Text
+              fontSize={{ base: "md", md: "xl", lg: "2xl" }}
+              position={"absolute"}
+              color={"design.300"}
+              paddingTop={{ base: 20, md: 36, lg: 48 }}
+              paddingLeft={{ base: 10, md: 12, lg: 20 }}
+              transition={"fade"}
+              transitionDuration={"600ms"}
+            >
+              {image.subHeading}
+            </Text>
+            <Image
+              height={"100%"}
+              width={"100%"}
+              objectFit={"cover"}
+              src={image.image}
+            />
+          </Flex>
+        ))}
+      </Slider>
     </>
   );
 }
