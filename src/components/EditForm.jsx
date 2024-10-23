a02fb3108d71b53fb041de5b7fd44131c6589266
mali0034 <nabhaanali8000@gmail.com>
2024-10-23 14:57:17 +0500

    addednrelevent edit action and loader form

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -18,12 +18,20 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import { Form, useLoaderData, useNavigate } from "react-router-dom";
+import {
+  Form,
+  redirect,
+  useLoaderData,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { PROJ_CATEGORIES } from "..";
+import { Formik, useFormik } from "formik";
 
-export default function EditForm({ selected, open, close }) {
+export default function EditForm({ selected, open, updateProjects }) {
   const { project } = useLoaderData();
-
+  const location = useLocation();
+  const redirectTo = location.state?.from;
   const [opened, setOpened] = useState(true);
   const navigate = useNavigate();
   //   useEffect(() => {
@@ -52,25 +60,24 @@ export default function EditForm({ selected, open, close }) {
 
   const handleClose = () => {
     setOpened(false);
-    navigate("..");
+
+    navigate(`${redirectTo}`);
   };
 
-  selected;
-  open;
   return (
     <>
-      <Form method="post">
-        <Modal
-          size={{ base: "xl", md: "5xl" }}
-          isOpen={opened}
-          motionPreset="slideInBottom"
-        >
-          <ModalOverlay />
-          <ModalContent top={"5%"}>
-            <ModalHeader>
-              <Heading>Edit Project</Heading>
-            </ModalHeader>
-            <ModalBody>
+      <Modal
+        size={{ base: "xl", md: "5xl" }}
+        isOpen={opened}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent top={"5%"}>
+          <ModalHeader>
+            <Heading>Edit Project</Heading>
+          </ModalHeader>
+          <ModalBody>
+            <Form method="patch">
               <FormControl>
                 <FormLabel>Title</FormLabel>
                 <Input
@@ -98,7 +105,7 @@ export default function EditForm({ selected, open, close }) {
               <FormControl>
                 <FormLabel>Category</FormLabel>
                 <Select
-                  defaultValue={project.projectCategories}
+                  defaultValue={project.projectCategory}
                   name="projectCategory"
                 >
                   {PROJ_CATEGORIES.map((category) => {
@@ -109,24 +116,67 @@ export default function EditForm({ selected, open, close }) {
                 </Select>
               </FormControl>
               <FormControl>
-                <FormLabel>Category</FormLabel>
-                <Select defaultValue={project.projectStatus}>
+                <FormLabel>Status</FormLabel>
+                <Select
+                  defaultValue={project.projectStatus}
+                  name="projectStatus"
+                >
                   <option value={"Ongoing"}>Ongoing</option>
                   <option value={"Completed"}>Completed</option>
                 </Select>
               </FormControl>
               <FormControl>
                 <FormLabel>Description</FormLabel>
-                <Textarea defaultValue={project.projectDescription}></Textarea>
+                <Textarea
+                  defaultValue={project.projectDescription}
+                  name="projectDescription"
+                ></Textarea>
               </FormControl>
+              <FormControl>
+                <Input
+                  type="hidden"
+                  defaultValue={redirectTo}
+                  name="redirect"
+                ></Input>
+              </FormControl>
+
               <Flex padding={4} justifyContent={"end"} gap={1}>
-                <Button type="submit">Submit</Button>
+                <Button type={"submit"}>Submit</Button>
                 <Button onClick={() => handleClose()}>Cancel</Button>
               </Flex>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </Form>
+            </Form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
+}
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+  console.log(data);
+
+  const updates = Object.fromEntries(data);
+
+  const response = await fetch("http://localhost:3000/projects/" + params.id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": null,
+    },
+    credentials: "include",
+    body: JSON.stringify(updates),
+  });
+
+  if (response.status === 404 || response.status === 400) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Issues" }, { status: 500 });
+  }
+
+  // return redirect("/Admin/Projects");
+  return redirect(updates.redirect);
 }
