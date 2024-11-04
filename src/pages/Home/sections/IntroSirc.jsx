d7085403845032011184d117937c073a230c6d20
mali0034 <nabhaanali8000@gmail.com>
2024-11-04 11:11:06 +0500

    added text to sirc home section and styling

--- src/pages/Home/sections/IntroSirc.jsx
+++ src/pages/Home/sections/IntroSirc.jsx
@@ -1,40 +1,57 @@
-import { Box, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import SIRC from "../../../assets/SIRC/SIRC2.png";
+import { Link } from "react-router-dom";
 export default function IntroSirc() {
   return (
     <>
       <Box
         minH={"32rem"}
         paddingBlock={{ base: 0, md: 12 }}
-        paddingLeft={16}
+        paddingLeft={14}
         bgColor={"design.500"}
       >
-        <SimpleGrid columns={2} paddingInline={6}>
+        <SimpleGrid columns={2} paddingInline={4}>
           <Box paddingRight={8}>
             <Heading
-              whiteSpace={"pre-line"}
               textColor={"brand.400"}
               size={"3xl"}
-              paddingBottom={4}
-              paddingLeft={{ base: 0, md: 4 }}
-              paddingRight={69}
+              paddingLeft={1}
+              paddingBottom={6}
+              paddingRight={68}
             >
               Small Island Research Center (SIRC)
             </Heading>
-            <Text>
+            <Text paddingBottom={2} paddingTop={2}>
               Located in Faresmaathoda in Huvadhoo Atoll of Maldives, the SIRC
               is for students and researchers in the field of marine sciences,
               island morphology and climate change.
             </Text>
-            <Text>
+            <Text paddingBottom={2}>
               The Research Center has worked closely with departments within
               Universities such as University of Auckland, New Zealand and
               University of Derby , United Kingdom.
             </Text>
-            <Text>
+
+            <Text paddingBottom={2}>
               The Center has two in-house programs, A summer coral reef ecology
               course and a coral reef system course.
             </Text>
+            <Text paddingBottom={2}>
+              The Center has facilitated several research papers and studies.
+            </Text>
+            <Link>
+              <Text fontWeight={600} _hover={{ textDecoration: "underline" }}>
+                {" "}
+                Learn More...
+              </Text>
+            </Link>
           </Box>
           <Box>
             <Image src={SIRC}></Image>
