5d1ea97a6f480f6d935ac2eeddb2e11888d74f3d
NabhaanCrowe <mohamed.nabhaan@crowe.mv>
2025-02-23 22:11:13 +0500

    sirc section fixed

--- src/pages/Home/sections/IntroSirc.jsx
+++ src/pages/Home/sections/IntroSirc.jsx
@@ -13,48 +13,51 @@ export default function IntroSirc() {
     <>
       <Box
         minH={"32rem"}
-        paddingBlock={{ base: 0, md: 12 }}
+        paddingBlock={{ base: 4, md: 12 }}
         paddingLeft={14}
         
+        
       >
-        <SimpleGrid columns={2} paddingInline={4}>
-          <Box paddingRight={8}>
+        <SimpleGrid columns={{base:1,lg:2}} paddingInline={4}>
+          <Box paddingRight={8} paddingBottom={{base:1,md:0}}>
             <Heading
+            textAlign={{base:'center',md:'left'}}
               textColor={"brand.400"}
-              size={"3xl"}
+              size={{base:'2xl',md:"3xl"}}
               paddingLeft={1}
-              paddingBottom={6}
+              paddingBottom={{base:3,md:6}}
               paddingRight={68}
             >
               Small Island Research Center (SIRC)
             </Heading>
-            <Text paddingBottom={2} paddingTop={2}>
+            <Text paddingBottom={2} paddingTop={{base:0,md:2}} fontSize={{base:'sm',md:"md"}} >
               Located in Faresmaathoda in Huvadhoo Atoll of Maldives, the SIRC
               is for students and researchers in the field of marine sciences,
               island morphology and climate change.
             </Text>
-            <Text paddingBottom={2}>
+            <Text paddingBottom={2} fontSize={{base:'sm',md:"md"}}>
               The Research Center has worked closely with departments within
               Universities such as University of Auckland, New Zealand and
               University of Derby , United Kingdom.
             </Text>
 
-            <Text paddingBottom={2}>
+            <Text paddingBottom={2} fontSize={{base:'sm',md:"md"}}> 
               The Center has two in-house programs, A summer coral reef ecology
               course and a coral reef system course.
             </Text>
-            <Text paddingBottom={2}>
+            <Text paddingBottom={2} fontSize={{base:'sm',md:"md"}}>
               The Center has facilitated several research papers and studies.
             </Text>
-            <Link to={"/SIRC"}>
-              <Text fontWeight={600} _hover={{ textDecoration: "underline" }}>
-                {" "}
+            <Link to={"/SIRC"} >
+              <Text fontSize={{base:"sm",md:"md"}} fontWeight={600} _hover={{ textDecoration: "underline" }}>
+                
                 Learn More...
               </Text>
             </Link>
           </Box>
-          <Box>
-            <Image src={SIRC}></Image>
+          <Box paddingRight={{base:16,md: 10}} >
+            <Image borderRadius
+            ={24} src={SIRC}></Image>
           </Box>
         </SimpleGrid>
       </Box>
