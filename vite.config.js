9863094eda5e2b94ebd4ed2448fa41b59e1c39d3
NabhaanCrowe <mohamed.nabhaan@crowe.mv>
2025-06-01 16:21:07 +0500

    Add render configuration and update Vite settings for improved build and server management. Introduced render.yaml for service definition and enhanced vite.config.js with build output directory, public directory, and server settings for better development and preview experience.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  publicDir: "public",
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  },
+});
